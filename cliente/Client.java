ccc3201111186a1abbf3abb10a9b94aa2f75ee71
YASMINCOS <costayasmin367@gmail.com>
2024-05-30 10:26:09 -0300

    mudando ip

--- cliente/Client.java
+++ cliente/Client.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 
 public class Client {
     public static void main(String[] args) {
-        try (Socket socket = new Socket("192.168.1.19", 12345)) {
+        try (Socket socket = new Socket("localhost", 12345)) {
             System.out.println("Conectado ao servidor!");
 
             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
