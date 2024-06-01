abcd67e577d3ee7b27457fa7aad2f0db008cbd5f
Pedro3148 <pedrohenrique3148@gmail.com>
2024-06-01 18:11:09 -0300

    Adicionando Menu Basico

--- src/main/java/cliente/Client.java
+++ src/main/java/cliente/Client.java
@@ -1,25 +1,52 @@
 package cliente;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Client {
     public static void main(String[] args) {
         try {
             Conexao conexao = new Conexao("localhost", 12346);
-            System.out.println("Conectado ao servidor!");
+            BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
-            conexao.enviar("LISTAR");
-            String responseLine;
-            while ((responseLine = conexao.receber()) != null) {
-                if (responseLine.equals("FIM_LISTA")) {
-                    break;
+            menu();
+            while (true) {
+                int op = Integer.parseInt(console.readLine());
+                if (op == 0) break;
+                switch (op) {
+                    case 1:
+                        conexao.enviar("LISTAR");
+                        break;
+                    case 2:
+                        System.out.println("Não operacional");
+                        break;
+                    case 3:
+                        System.out.println("Não operacional");
+                        break;
+                    case 4:
+                        System.out.println("Não operacional");
+                        break;
+                    default:
+                        System.out.println("Opção invalida");
                 }
-                System.out.println(responseLine);
+                System.out.println(conexao.receber());
             }
-
+            conexao.fechar();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void menu() {
+        System.out.println("====== MENU ======");
+        System.out.println("1 - LISTAR");
+        System.out.println("2 - ALUGAR");
+        System.out.println("3 - DEVOLVER");
+        System.out.println("4 - CADASTRAR");
+        System.out.println("0 - SAIR");
+        System.out.println("==================");
+        System.out.println("Opção:");
     }
 }
 
