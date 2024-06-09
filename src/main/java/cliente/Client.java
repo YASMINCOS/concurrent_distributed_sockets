e3b2759984892cec31164cbdbd956423086cc1a1
kauamiguel <kauamcm@gmail.com>
2024-06-09 08:46:37 -0300

    Refatorando a classe cliente

--- src/main/java/cliente/Client.java
+++ src/main/java/cliente/Client.java
@@ -16,52 +16,69 @@ public class Client {
                 if (op == 0) break;
                 switch (op) {
                     case 1:
-                        conexao.enviar("LISTAR");
-                        String resposta;
-                        while (!(resposta = conexao.receber()).equals("FIM_LISTA")) {
-                            System.out.println(resposta);
-                        }
+                        enviarLivros(conexao, console);
                         break;
                     case 2:
-                        System.out.println("Digite o nome do livro que deseja alugar: ");
-                        String tituloAlugar = console.readLine();
-                        String comandoAlugar = String.format("ALUGAR %s", tituloAlugar);
-                        System.out.println("Enviando comando: " + comandoAlugar);
-                        conexao.enviar(comandoAlugar);
-                        String respostaAluguel = conexao.receber();
-                        if (respostaAluguel.startsWith("ALERTA:")) {
-                            System.out.println(respostaAluguel);
-                        } else {
-                            System.out.println(respostaAluguel);
-                        }
+                        alugarLivro(conexao, console);
                         break;
                     case 3:
-                        System.out.println("Digite o nome do livro que deseja devolver: ");
-                        String tituloDevolver = console.readLine();
-                        String comandoDevolver = String.format("DEVOLVER %s", tituloDevolver);
-                        System.out.println("Enviando comando: " + comandoDevolver);
-                        conexao.enviar(comandoDevolver);
+                        devolverLivro(conexao, console);
                         break;
                     case 4:
-                        System.out.println("Digite o título do livro:");
-                        String titulo = console.readLine();
-                        System.out.println("Digite o autor do livro:");
-                        String autor = console.readLine();
-                        System.out.println("Digite o gênero do livro:");
-                        String genero = console.readLine();
-                        System.out.println("Digite a quantidade de exemplares:");
-                        int exemplares = Integer.parseInt(console.readLine());
-                        String comando = String.format("CADASTRAR %s, %s, %s, %d", titulo, autor, genero, exemplares);
-                        System.out.println("Enviando comando: " + comando);
-                        conexao.enviar(comando);
+                        cadastrarLivros(conexao, console);
                         break;
                     default:
                         System.out.println("Opção inválida");
+                        break;
                 }
             }
             conexao.fechar();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void cadastrarLivros(Conexao conexao, BufferedReader console) throws IOException{
+        System.out.println("Digite o título do livro:");
+        String titulo = console.readLine();
+        System.out.println("Digite o autor do livro:");
+        String autor = console.readLine();
+        System.out.println("Digite o gênero do livro:");
+        String genero = console.readLine();
+        System.out.println("Digite a quantidade de exemplares:");
+        int exemplares = Integer.parseInt(console.readLine());
+        String comando = String.format("CADASTRAR %s, %s, %s, %d", titulo, autor, genero, exemplares);
+        System.out.println("Enviando comando: " + comando);
+        conexao.enviar(comando);
+    }
+
+    public static void devolverLivro(Conexao conexao, BufferedReader console) throws IOException{
+        System.out.println("Digite o nome do livro que deseja devolver: ");
+        String tituloDevolver = console.readLine();
+        String comandoDevolver = String.format("DEVOLVER %s", tituloDevolver);
+        System.out.println("Enviando comando: " + comandoDevolver);
+        conexao.enviar(comandoDevolver);
+    }
+
+    public static void alugarLivro(Conexao conexao, BufferedReader console) throws IOException {
+        System.out.println("Digite o nome do livro que deseja alugar: ");
+        String tituloAlugar = console.readLine();
+        String comandoAlugar = String.format("ALUGAR %s", tituloAlugar);
+        System.out.println("Enviando comando: " + comandoAlugar);
+        conexao.enviar(comandoAlugar);
+        String respostaAluguel = conexao.receber();
+        if (respostaAluguel.startsWith("ALERTA:")) {
+            System.out.println(respostaAluguel);
+        } else {
+            System.out.println(respostaAluguel);
+        }
+    }
+
+    public static void enviarLivros(Conexao conexao, BufferedReader console) throws IOException {
+        conexao.enviar("LISTAR");
+        String resposta;
+        while (!(resposta = conexao.receber()).equals("FIM_LISTA")) {
+            System.out.println(resposta);
         }
     }
 
